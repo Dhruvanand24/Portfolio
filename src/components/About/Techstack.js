d6869256b13c20ff875aac75783231acda233e10
Dhruv_Anand <ananddhruv475@gmail.com>
2023-11-03 12:41:48 +0530

    a

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; 
 import {
   DiJavascript1,
   DiReact,
@@ -18,27 +20,41 @@ import { TbBrandGolang } from "react-icons/tb";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+     <Tippy content={<span>C++</span>}> 
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
+      </Tippy>
+      <Tippy content={<span>JavaScript</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
+      </Tippy>
+      <Tippy content={<span>ReactJS</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
+      </Tippy>
+      <Tippy content={<span>Git</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
+      </Tippy>
+      <Tippy content={<span>Firebase</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
+      </Tippy>
+      <Tippy content={<span>Python</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
+      </Tippy>
+      <Tippy content={<span>MySql</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <DiMysql />
       </Col>
+      </Tippy>
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; 
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
@@ -11,15 +13,21 @@ import {
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Tippy content={<span>Visual Studio Code</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
+      </Tippy>
+      <Tippy content={<span>Slack</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
       </Col>
+      </Tippy>
+      <Tippy content={<span>Vercel</span>}>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
+      </Tippy>
     </Row>
   );
 }
